bf5a0c58b1d42acf722cde3cc2b3a880b8d0dd1b
YasserElHaddar16 <Yasser.El.Haddar@mindtrace.ai>
2024-12-29 19:56:41 +0000

    update dependencies

--- setup.py
+++ setup.py
@@ -81,9 +81,14 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-requirements = ["torch>=1.1.0", "numba", "numpy<=1.21", "scikit-learn"]
+requirements = [
+    "torch>=1.1.0",
+    "numba",
+    "numpy>=1.20,<2.0",
+    "scikit-learn"
+]
 
-url = "https://github.com/nicolas-chaulet/torch-points-kernels"
+url = "https://github.com/YasserElHaddar16/torch-points-kernels"
 __version__ = "0.7.1"
 setup(
     name="torch-points-kernels",
@@ -98,6 +103,7 @@ setup(
     cmdclass=get_cmdclass(),
     long_description=long_description,
     long_description_content_type="text/markdown",
+    python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
